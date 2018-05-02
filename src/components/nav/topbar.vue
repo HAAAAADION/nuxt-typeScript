ebd99123878b46127b518b24fd754c7910e982f5
haohui.zhou <fhlie11@qq.com>
2018-05-02 11:07:39 +0800

    init

---
+++ src/components/nav/topbar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="topbar">
+        <div class="topbar__logo">
+            <img src="//24haowan-cdn.shanyougame.com/public/24HaoWan/web/logo-white.png" />
+            <span class="topbar__title">| 代理商管理后台</span>
+        </div>
+        <div class="topbar__user-info">
+            <div class="topbar__user-icon">
+                <span class="u-icon u-icon--personal-center"></span>
+                <span class="topbar__user-name">Penn</span>
+            </div>
+            <a>注销</a>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .topbar {
+        background: #1AB899;
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 20px;
+        box-sizing: border-box;
+        color: #fff;
+
+        &__logo {
+            display: flex;
+            align-items: center;
+
+            img {
+                height: 34px;
+            }
+        }
+
+        &__title {
+            margin-left: 5px;
+        }
+
+        &__user-info {
+            display: flex;
+        }
+
+        &__user-icon img{
+            width: 14px;
+            height: 14px;
+            margin-right: 10px;
+        }
+
+        &__user-name {
+            margin-left: 10px;
+        }
+
+        a {
+            display: inline-block;
+            margin-left: 20px;
+        }
+    }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,81 +1,43 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo :a="'quyuy'" />
-      <h1 class="title">
-        {{ msg }}
-      </h1>
-      <h2 class="subtitle">
-        {{ asd }}
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <div class="container">
+        <div>
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+                <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+                <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
     </div>
-  </section>
 </template>
 
 <script lang="ts">
 import {
-  Component,
-  Vue
+    Component,
+    Vue
 } from "nuxt-property-decorator"
-import AppLogo from '@/components/AppLogo.vue'
-// import asd from '@/assets/common-config.json'
-import qqq from '@/lib/store/io/games'
+import {
+    Breadcrumb as ElBreadcrumb,
+    BreadcrumbItem as ElBreadcrumbItem,
+} from 'element-ui';
 
 @Component({
-  components: {
-    AppLogo
-  }
+    components: {
+        ElBreadcrumb,
+        ElBreadcrumbItem
+    }
 })
 export default class extends Vue {
-  public msg: string = 'qwe'
-  get asd (): string {
-    return 'qwe'
-  }
-  async mounted() {
-    const a = await qqq.getSelected()
-    // console.log('qweqwe: ', asd.topbar);
-    console.log(a);
-  }
+    public msg: string = 'qwe'
+    get asd (): string {
+        return 'qwe'
+    }
+    async mounted() {
+    }
 }
 </script>
 
 <style>
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
-  }
+    .container {
+    }
 </style>
